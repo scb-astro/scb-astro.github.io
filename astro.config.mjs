068aef391e006fec991042d6e0dc54943d2488c5
Alimanco <s41v4c@gmail.com>
2026-07-03 11:56:14 +0100

    Updated site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+
 export default defineConfig({
   site: 'https://scb-astro.github.io',
-  base: '/astro-scholar/',
+  base: '/scb-astro.github.io/',
 });
